0d58c595dab67582ce4c1f03cf4e6b10fac9e3b4
Ajmal Noushad <ajufhn@gmail.com>
2018-09-06 22:13:59 +0530

    Added loader & word break

--- src/App.js
+++ src/App.js
@@ -2,35 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ReactLoading from 'react-loading';
+
 const url = 'https://kerala-university-api.herokuapp.com'
 
 class App extends Component {
   state = {
-    datas: []
+    datas: [],
+    loading: true,
   }
+  
   componentDidMount() {
+    console.log(this.header)
     fetch(url).then(resp => {
       resp.json().then(datas => {
-        this.setState({datas})
+        this.setState({ datas, loading: false })
       })
     })
   }
+
+  header = React.createRef();
+
   render() {
-    const {datas} = this.state;
+    const { datas, loading } = this.state;
+    
     return (
       <div className="App">
-        <header className="App-header">
+        <header ref={this.header} className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">University results</h1>
+          <h1 className="App-title">University Results</h1>
         </header>
         <div className="App-intro">
           {
-            datas.map(data => 
-              <div className="ListItem">
-                <h4 className="Text">{data.title}</h4>
-                <a className="DownloadButton" href={data.link}><button>Download</button></a>
+            loading ?
+              <div className='container' style={{ minHeight: window.innerHeight - 200, display: 'flex', alignItems:'center', justifyContent: 'center', flexDirection:'column' }}>
+                <ReactLoading type={'spin'} color={'blue'} height={50} width={50} className='loader' />
               </div>
-            )
+              :
+              <div>
+                {
+                  datas.map(data =>
+                    <div className="ListItem">
+                      <h4 className="Text">{data.title}</h4>
+                      <a className="DownloadButton" href={data.link}><button>Download</button></a>
+                    </div>
+                  )
+                }
+              </div>
           }
         </div>
       </div>
